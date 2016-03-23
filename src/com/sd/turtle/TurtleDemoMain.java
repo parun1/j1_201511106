55e8db70459faaae8ff149a4768521f4093c1f42
parun1 <gjgj74@naver.com>
2016-03-24 00:05:08 +0900

    HW

---
+++ src/com/sd/turtle/TurtleDemoMain.java
@@ -0,0 +1,21 @@
+package com.sd.turtle;
+import ch.aplu.turtle.*;
+
+class TurtleDemoMain {
+  Turtle t1 = new Turtle();
+
+  TurtleDemoMain() {
+    t1.forward(100);
+    t1.right(90);
+    t1.forward(100);
+    t1.right(90);
+    t1.forward(100);
+    t1.right(90);
+    t1.forward(100);
+    t1.right(90);
+  }
+
+  public static void main(String[] args) {
+    new TurtleDemoMain();
+  }
+}
